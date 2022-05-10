030a7488cfdb55d5a672e960afecd2058cf797ab
exoutia <jhav365@gmail.com>
2022-05-10 07:56:56 +0530

    created connect_node function

--- 2_create_crypto/create_crypto.py
+++ 2_create_crypto/create_crypto.py
@@ -161,9 +161,22 @@ def add_transaction():
     
 #* Part 3 - Decentralising our blockchain 
 
+# Connecting new nodes
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('node')
+    if nodes is None:
+        return 'No node', 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are connected. The vivecoin Blockchain now contains the following nodes:',
+                'total_nodes':list(blockchain.nodes)}
+    return jsonify(response), 201
+        
+
 # Running the app
 app.run(host ='0.0.0.0', port=5000)
-
 
 
 
