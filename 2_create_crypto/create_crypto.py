ebc50378a461a57fa0958a19fcb81baa7dc5e1ba
exoutia <jhav365@gmail.com>
2022-05-10 17:55:55 +0530

    Replacing the chain by the longest chain if needed

--- 2_create_crypto/create_crypto.py
+++ 2_create_crypto/create_crypto.py
@@ -173,7 +173,19 @@ def connect_node():
     response = {'message': 'All the nodes are connected. The vivecoin Blockchain now contains the following nodes:',
                 'total_nodes':list(blockchain.nodes)}
     return jsonify(response), 201
+
+# Replacing the chain by the longest chain if needed
+@app.route('/replace_chain', methods=['GET'])
+def replace_chain():
+    is_chain_replaced = blockchain.replace_chain()
+    if is_chain_replaced:
+        response = {'message': 'The nodes had different chains so the chain was replaced.',
+                    'new_chain': blockchain.chain}
         
+    else:
+        response = {'message': 'All good, chain is the largest one.', 
+                    'chain': blockchain.chain}
+    return jsonify(response), 200
 
 # Running the app
 app.run(host ='0.0.0.0', port=5000)
--- README.md
+++ README.md
@@ -3,6 +3,12 @@
 This is a personal project to code a functional blockchain with a cryptocurrency and to better understand inner working of this technology 
 that if utilised correctly can change the world for good.
 
+I have used:
+    Flask(Python Library)
+    Postman(Application)
+    
+
+
 
 
 
